1b8ebdc08932c24a460dee0208cd75802f08b165
Nicolas Lecourtois <lecourtoisn@gmail.com>
2016-03-16 15:44:40 +0100

    Split code into modules

---
+++ carts/src/main/java/fr/unice/polytech/isa/tcf/components/carts/CartStatelessBean.java
@@ -0,0 +1,31 @@
+package fr.unice.polytech.isa.tcf.components.carts;
+
+import fr.unice.polytech.isa.tcf.components.CartBean;
+import fr.unice.polytech.isa.tcf.entities.Customer;
+import fr.unice.polytech.isa.tcf.entities.Item;
+import fr.unice.polytech.isa.tcf.utils.Database;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Stateless(name = "cart-stateless")
+public class CartStatelessBean extends CartBean {
+
+	@EJB
+	private Database memory;
+
+	@Override
+	public boolean add(Customer c, Item item) {
+		memory.getCarts().put(c, updateCart(c, item));
+		return true;
+	}
+
+	@Override
+	public Set<Item> contents(Customer c) {
+		return memory.getCarts().getOrDefault(c, new HashSet<Item>());
+	}
+
+}

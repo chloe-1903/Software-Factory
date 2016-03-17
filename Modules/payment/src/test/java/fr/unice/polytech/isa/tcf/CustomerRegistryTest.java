6d39301383bc97762d0e24904d00845b9ea23f12
Chloé GUGLIELMI <guglielmi.chloe@etu.unice.fr>
2016-03-17 14:40:38 +0100

    abstractTest dependencies

--- Modules/payment/src/test/java/fr/unice/polytech/isa/tcf/CustomerRegistryTest.java
+++ Modules/payment/src/test/java/fr/unice/polytech/isa/tcf/CustomerRegistryTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import javax.ejb.EJB;
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 

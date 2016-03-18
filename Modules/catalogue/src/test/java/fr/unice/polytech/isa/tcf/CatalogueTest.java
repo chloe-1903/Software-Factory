0cc28bfbdaea8326aaa6df0a2f69f9e13b6e0d8e
Chloé GUGLIELMI <guglielmi.chloe@etu.unice.fr>
2016-03-18 10:14:19 +0100

    Résolution cycle2: ArquilianTests coupé en 2

--- Modules/catalogue/src/test/java/fr/unice/polytech/isa/tcf/CatalogueTest.java
+++ Modules/catalogue/src/test/java/fr/unice/polytech/isa/tcf/CatalogueTest.java
@@ -1,6 +1,5 @@
 package fr.unice.polytech.isa.tcf;
 
-import arquillian.AbstractTCFTest;
 import fr.unice.polytech.isa.tcf.entities.Cookies;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
--- Modules/items/src/main/java/fr/unice/polytech/isa/tcf/interceptors/ItemVerifier.java
+++ Modules/items/src/main/java/fr/unice/polytech/isa/tcf/interceptors/ItemVerifier.java
@@ -17,5 +17,4 @@ public class ItemVerifier {
 
 		return ctx.proceed();
 	}
-
 }

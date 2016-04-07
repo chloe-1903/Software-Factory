7e4a3cece2d4f80aa2e894317a4bef4082a65c49
Nicolas <lecourtoisn@gmail.com>
2016-04-08 01:53:07 +0200

    Fixed AbstractTCFTest

--- Modules/webserv/src/test/java/arquillian/AbstractTCFTest.java
+++ Modules/webserv/src/test/java/arquillian/AbstractTCFTest.java
@@ -1,15 +1,15 @@
 package arquillian;
 
-import fr.unice.polytech.isa.tcf.CustomerFinder;
-import fr.unice.polytech.isa.tcf.CustomerRegistration;
-import fr.unice.polytech.isa.tcf.OrderProcessing;
-import fr.unice.polytech.isa.tcf.Payment;
+import fr.unice.polytech.isa.tcf.*;
 import fr.unice.polytech.isa.tcf.components.CashierBean;
 import fr.unice.polytech.isa.tcf.components.CustomerRegistryBean;
 import fr.unice.polytech.isa.tcf.components.KitchenBean;
+import fr.unice.polytech.isa.tcf.components.carts.CartStatefulBean;
+import fr.unice.polytech.isa.tcf.components.carts.CartStatelessBean;
 import fr.unice.polytech.isa.tcf.entities.Customer;
 import fr.unice.polytech.isa.tcf.exceptions.AlreadyExistingCustomerException;
 import fr.unice.polytech.isa.tcf.exceptions.PaymentException;
+import fr.unice.polytech.isa.tcf.interceptors.CartCounter;
 import fr.unice.polytech.isa.tcf.interceptors.ItemVerifier;
 import fr.unice.polytech.isa.tcf.interceptors.Logger;
 import fr.unice.polytech.isa.tcf.utils.BankAPI;
@@ -37,18 +37,18 @@ public abstract class AbstractTCFTest {
 				// Entities
 				.addPackage(Customer.class.getPackage())
 				// Components Interfaces
-	//			.addPackage(CartModifier.class.getPackage())
+				.addPackage(CartModifier.class.getPackage())
 				.addPackage(CustomerRegistration.class.getPackage())
 				.addPackage(CustomerFinder.class.getPackage())
 				.addPackage(Payment.class.getPackage())
 				.addPackage(OrderProcessing.class.getPackage())
 				// Cart components
-				//.addPackage(CartStatefulBean.class.getPackage())
-				//.addPackage(CartStatelessBean.class.getPackage())
+				.addPackage(CartStatefulBean.class.getPackage())
+				.addPackage(CartStatelessBean.class.getPackage())
 				// Interceptors
 				.addPackage(Logger.class.getPackage())
 				.addPackage(ItemVerifier.class.getPackage())
-				//.addPackage(CartCounter.class.getPackage())
+				.addPackage(CartCounter.class.getPackage())
 				// Exceptions
 				.addPackage(AlreadyExistingCustomerException.class.getPackage())
 				.addPackage(PaymentException.class.getPackage())
